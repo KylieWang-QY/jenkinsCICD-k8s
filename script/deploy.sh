246cfe6d27f8f7a1e1498ea7b6f20ff3d8d52e9d
Kylie <wangqianyv@gmail.com>
2022-12-02 12:19:12 +1030

    health check

--- script/deploy.sh
+++ script/deploy.sh
@@ -19,3 +19,27 @@ echo "ready to apply"
 kubectl apply -f web-demo-deploy.yaml
 
 echo "apply success"
+
+# HEALTH CKECK
+count=60
+success=0
+IFS=','
+sleep 5
+while [$[count] -gt 0]
+do 
+  replicas=$(kubectl get deploy ${name} -o go-template='{{.status.replicas}},{{.status.updatedReplicas}}.{{.status.readyReplicas}}.{{.status.availabeReplicas}}')
+  echo "replicas: ${replicas}"
+  arr=(${replicas})
+  if [ "${arr[0]}" == "${arr[1]}" -a "${arr[1]}" == "${arr[2]}" -a "${arr[2]}" == "${arr[3]}"];then
+    echo "health check success"
+    success = 1
+    break
+  fi
+  ((count--))
+  sleep 2
+done
+
+if [ ${success} -ne 1];then
+  echo "health check failed!"
+  exit 1
+fi
